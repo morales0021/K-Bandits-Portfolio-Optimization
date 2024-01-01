12d0a9044bd94e1b4d8736fb77f275ccde484832
Jose <morales0021@gmail.com>
2024-01-01 23:55:44 +0100

    updates in alpha an ubc years computations

--- exploration/explore.py
+++ exploration/explore.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import pdb
 
 class explore:
 
     def __init__(self, method):
 
         self.method     = method
-        self.rest_compo = None
+        self.rets_compo = None
         self.rets_added = None
         self.df_sim     = None
 
@@ -17,13 +18,16 @@ class explore:
         self.df_sim = pd.DataFrame()
 
         for k in range(sims):
+            try:
+                rewards,Q,N = self.method(**kwargs)
+                df_rwd = pd.DataFrame(rewards)
+                ret_com = ((df_rwd+1).cumprod().iloc[-1] - 1)[0]
+                ret_sum = df_rwd.sum()[0]
 
-            rewards,Q,N = self.method(**kwargs)
-            df_rwd = pd.DataFrame(rewards)
-            ret_com = ((df_rwd+1).cumprod().iloc[-1] - 1)[0]
-            ret_sum = df_rwd.sum()[0]
+                self.rets_compo[k] = ret_com
+                self.rets_added[k] = ret_sum
+
+                self.df_sim = pd.concat([self.df_sim,df_rwd], axis = 1)
             
-            self.rets_compo[k] = ret_com
-            self.rets_added[k] = ret_sum
-            
-            self.df_sim = pd.concat([self.df_sim,df_rwd], axis = 1) 
+            except Exception as e :
+                pdb.set_trace()
